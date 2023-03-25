a2da0c43e91593294da7618d5f2a79d51f6d5e91
Nikita Kravchenko <nickravchenko98@gmail.com>
2023-03-24 20:21:47 -0400

    Uploading zips or json files

--- page_uploader/uploader.py
+++ page_uploader/uploader.py
@@ -2,20 +2,54 @@
 import json
 from core.models import Page
 import time
+import zipfile
+import os
+
+
+def saveJsonFile(file):
+    pages_data = json.loads(file.read())
+    # upload_pages(pages_data)
+    print("------------------------New File------------------------")
+    print(pages_data)
+
+def processFile(file):
+    filename = file.name
+    file_extension = os.path.splitext(filename)[1].lower()
+
+    if file_extension == '.zip':
+        with zipfile.ZipFile(file) as archive:
+            for name in archive.namelist():
+                file = archive.open(name)
+
+                if file:
+                    processFile(file)
+
+            return
+
+    if file_extension == '.json':
+        saveJsonFile(file)
+        return
 
 def upload_pages(pages_data):
     """Uploads pages to the database."""
 
     for url, page_data in pages_data.items():
-        page, created = Page.objects.get_or_create(url=url)
+        try:
+            page, created = Page.objects.get_or_create(url=url)
 
-        page.title = page_data['title']
-        page.headers = page_data['headings']
-        page.text = page_data['body']
+            page.title = page_data['title']
+            page.headers = page_data['headings']
+            page.text = page_data['body']
 
-        page.save()
+            if page.title == "No title":
+                page.delete()
+                continue
 
-        if created:
-            print(f"Created new page: {url}")
-        else:
-            print(f"Updated page: {url}")
+            page.save()
+
+            if created:
+                print(f"Created new page: {url}")
+            else:
+                print(f"Updated page: {url}")
+        except Exception as e:
+            print(f"Error uploading page: {url} - {e}")
--- page_uploader/views.py
+++ page_uploader/views.py
@@ -1,38 +1,42 @@
+import os
 from django.db.models import Q
 from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
-from drf_spectacular.types import OpenApiTypes
 
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from core.models import Page
-from search.serializers import PageSerializer
-from page_uploader.uploader import upload_pages
-import json
+from page_uploader.uploader import processFile
+
+
 
 
 class UploadView(APIView):
     """Uploads pages to the database."""
 
     @extend_schema(
-        operation_id="upload_file",
         request={
             "multipart/form-data": {
                 "type": "object",
-                "properties": {"file": {"type": "string", "format": "binary"}},
-            }
+                "properties": {
+                    "files": {
+                        "type": "array",
+                        "description": "Files to upload. .zip for archives, .json for single files",
+                        "items": {
+                            "type": "string",
+                            "format": "binary"
+                        }
+                    }
+                },
+            },
         },
     )
     def put(self, request, format=None):
         """Uploads pages to the database."""
         try:
-            file_obj = request.FILES.get("file")
-            pages_data = json.loads(file_obj.read())
-            if file_obj:
-                upload_pages(pages_data)
+            for file in request.FILES.getlist('files'):
+                processFile(file)
 
-            return Response({"results": "Pages uploaded"})
+            return Response({"message": "Files uploaded successfully"})
         except Exception as e:
             print(e)
             return Response({"error": str(e)})
-
